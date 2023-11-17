83a6618d9eb7ce2d13995eb96d8fc42169ea1d2d
Fahad Maqsood Qazi <50149692+fahadmaqsood@users.noreply.github.com>
2023-11-17 16:33:13 +0500

    Add files via upload

--- src/components/AboutUsComponent.js
+++ src/components/AboutUsComponent.js
@@ -5,18 +5,18 @@ const AboutUsComponent = () => {
     <div className="row p-5 about-us">
       
       <div className="row d-flex align-items-center">
-          <div className="col-8">
+          <div className="col-9">
             <h3>About Us</h3>
           </div>
 
-          <div className="col-4">
+          <div className="col-3">
             <img src="/image-3@2x.png" />
           </div>
       </div>
 
       <br />
        
-      <div className="col-12">
+      <div className="col-12 text">
         <p>At our bakery, baking isn't just a process; it's an art, a passion that's woven into every loaf, pastry, and confection we create. Established with a vision to redefine indulgence, we take pride in crafting experiences that transcend the ordinary. Our story began with a small kitchen and a big dream – to infuse the world with the warmth of freshly baked goods. 
 
 What sets us apart is our unwavering commitment to quality. We meticulously source the finest ingredients, from velvety chocolate to locally grown fruits, ensuring that every bite tells a tale of exceptional flavor. But it's not just about ingredients; it's about the hands that shape them. Our dedicated team of master bakers pours their heart into each recipe, combining time-honored techniques with innovative twists.
